66a686fefe829a7461ecc06e8f537dfd9029fa7d
Ravi Sankar Karuturi <ravisankarkaruturi@gmail.com>
2016-07-13 10:24:23 +0530

    Started configuring web services

---
+++ src/com/smartway/core/managebeans/UserInfoMaster.java
@@ -0,0 +1,17 @@
+package com.smartway.core.managebeans;
+
+import java.util.Collection;
+
+import com.smartway.core.model.UserInfo;
+public class UserInfoMaster {
+	private Collection<UserInfo> dataList;
+
+	public Collection<UserInfo> getDataList() {
+		return dataList;
+	}
+
+	public void setDataList(Collection<UserInfo> dataList) {
+		this.dataList = dataList;
+	}
+	
+}
---
+++ src/com/smartway/core/utils/Common.java
@@ -0,0 +1,26 @@
+package com.smartway.core.utils;
+
+import java.io.IOException;
+
+import javax.xml.bind.JAXBException;
+
+import org.codehaus.jackson.JsonParseException;
+import org.codehaus.jackson.map.JsonMappingException;
+import org.codehaus.jackson.map.ObjectMapper;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+public class Common {
+	public JSONObject pojo2Json(Object obj) throws JAXBException,
+	JsonParseException, JsonMappingException, IOException,
+	ParseException 
+	{
+		JSONObject object = new JSONObject();
+		ObjectMapper objectMapper = new ObjectMapper();
+		String jsonString = objectMapper.writeValueAsString(obj);
+		JSONParser jsonParser = new JSONParser();
+		object = (JSONObject) jsonParser.parse(jsonString);
+		return object;
+	}
+}
