c2728097fa86b8ddd77b107d5b5141cab5dc8da9
Ravi Sankar Karuturi <ravisankarkaruturi@gmail.com>
2016-07-25 16:18:42 +0530

    updated the pojo to json functionality

--- src/com/smartway/core/utils/Common.java
+++ src/com/smartway/core/utils/Common.java
@@ -2,28 +2,28 @@ package com.smartway.core.utils;
 
 import java.io.IOException;
 
-import javax.xml.bind.JAXBException;
-
-import org.codehaus.jackson.JsonParseException;
+import org.codehaus.jackson.JsonGenerationException;
 import org.codehaus.jackson.map.JsonMappingException;
 import org.codehaus.jackson.map.ObjectMapper;
-import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 
 public class Common {
-	public JSONObject pojo2Json(Object obj) throws JAXBException,
-	JsonParseException, JsonMappingException, IOException,
-	ParseException 
+	public JSONObject pojo2Json(Object obj) throws  JsonGenerationException, JsonMappingException, IOException, ParseException 
 	{
-		JSONObject object = new JSONObject();
+		/*Gson gson = new GsonBuilder().create();
+		String json = gson.toJson(obj);*/
 		ObjectMapper objectMapper = new ObjectMapper();
 		String jsonString = objectMapper.writeValueAsString(obj);
-		JSONParser jsonParser = new JSONParser();
-		//object = (JSONObject) jsonParser.parse(jsonString);
-		object.put("list", obj);
-		System.out.println("-->"+object.toJSONString());
+		JSONParser parser = new JSONParser();
+		JSONObject jsonobj = (JSONObject) parser.parse(jsonString);
+		return jsonobj;
+	}
+	public JSONObject pojo2Json(Object obj, String key) 
+	{
+		JSONObject object = new JSONObject();
+		object.put(key,obj);
 		return object;
 	}
 }
