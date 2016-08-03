9e8c9add969c91b72c9878168d12677ab330e68b
Ravi Sankar Karuturi <ravisankarkaruturi@gmail.com>
2016-08-03 17:48:04 +0530

    updated new rest service for device information

--- src/com/smartway/core/utils/GenerateID.java
+++ src/com/smartway/core/utils/GenerateID.java
@@ -6,7 +6,6 @@ import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import com.smartway.core.model.GenItemid;
-import com.smartway.core.model.ListUserDevice;
 import com.smartway.core.mysql.service.GenericService;
 
 
